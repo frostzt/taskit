ec21f9160e78a81c8796b58081e08eeb1e514fed
frostzt <aidenfrostbite@gmail.com>
2021-06-29 18:21:36 +0530

    got the basic user signup ready

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,5 +1,20 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UsersRepository extends Repository<User> {}
+export class UsersRepository extends Repository<User> {
+  public async createUser(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const newUser = this.create({
+      username,
+      password,
+    });
+
+    await this.save(newUser);
+    return;
+  }
+}
