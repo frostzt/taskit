7da539b0c2efc7d0b691ae23d5a47b891dbb8406
frostzt <aidenfrostbite@gmail.com>
2021-07-02 12:51:23 +0530

    implemented logging with Nestjs Logger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,17 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformerInterceptor } from './transform.interceptor';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformerInterceptor());
-  await app.listen(3000);
+
+  const PORT = 3000;
+
+  await app.listen(PORT);
+  logger.log(`Application running on port ${PORT}`);
 }
 bootstrap();
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  Logger,
   Param,
   Patch,
   Post,
@@ -21,6 +22,7 @@ import { GetUser } from 'src/auth/get-user.decorator';
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+  private logger = new Logger('TaskController');
   constructor(private tasksService: TasksService) {}
 
   @Get()
@@ -28,6 +30,7 @@ export class TasksController {
     @Query() filterDto: GetTaskFilterDto,
     @GetUser() user: User,
   ): Promise<Task[]> {
+    this.logger.verbose(`User ${user.username} get all tasks!`);
     return this.tasksService.getTasks(filterDto, user);
   }
 
