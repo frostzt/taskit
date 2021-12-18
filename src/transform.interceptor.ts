d9e45550eebb714ee872b2191f617d9678976a45
frostzt <aidenfrostbite@gmail.com>
2021-12-18 17:14:38 +0530

    refactor: 💡 changed and updated outdated imports

--- src/transform.interceptor.ts
+++ src/transform.interceptor.ts
@@ -4,12 +4,12 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
-import { classToPlain } from 'class-transformer';
+import { instanceToPlain } from 'class-transformer';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 export class TransformerInterceptor implements NestInterceptor {
   intercept(_context: ExecutionContext, next: CallHandler<any>) {
-    return next.handle().pipe(map((data) => classToPlain(data)));
+    return next.handle().pipe(map((data) => instanceToPlain(data)));
   }
 }
