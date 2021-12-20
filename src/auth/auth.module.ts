998c84f6062f315ec01756dc1fb2c63941418b57
frostzt <aidenfrostbite@gmail.com>
2021-12-20 20:38:09 +0530

    increased jwtExpiry from 1 hour to 5 days

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -19,7 +19,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         return {
           secret: configService.get('JWT_SECRET'),
           signOptions: {
-            expiresIn: 3600,
+            expiresIn: '432000s',
           },
         };
       },
